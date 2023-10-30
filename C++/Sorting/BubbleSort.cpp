9facded281bd2e9a345886b97b4eb2ad55dea236
Aryan Sharma <128162387+Aryan741x@users.noreply.github.com>
2023-10-31 00:05:29 +0530

    Create BubbleSort.cpp

---
+++ C++/Sorting/BubbleSort.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+void bubbleSort(int arr[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        bool swapped = false;
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                std::swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+        if (!swapped) {
+            break; // If no swaps were made in a pass, the array is already sorted
+        }
+    }
+}
+
+int main() {
+    using namespace std;
+
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Original array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    bubbleSort(arr, size);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
