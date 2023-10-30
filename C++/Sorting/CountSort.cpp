bf0e176bad5849138454a030ec502e09f7c9a023
Aryan Sharma <128162387+Aryan741x@users.noreply.github.com>
2023-10-31 00:08:33 +0530

    Create CountSort.cpp

---
+++ C++/Sorting/CountSort.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+
+void countingSort(int arr[], int size) {
+    int max = arr[0];
+    for (int i = 1; i < size; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    int min = arr[0];
+    for (int i = 1; i < size; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+
+    int range = max - min + 1;
+    int* count = new int[range]{0};
+    int* output = new int[size];
+
+    for (int i = 0; i < size; i++) {
+        count[arr[i] - min]++;
+    }
+
+    for (int i = 1; i < range; i++) {
+        count[i] += count[i - 1];
+    }
+
+    for (int i = size - 1; i >= 0; i--) {
+        output[count[arr[i] - min] - 1] = arr[i];
+        count[arr[i] - min]--;
+    }
+
+    for (int i = 0; i < size; i++) {
+        arr[i] = output[i];
+    }
+
+    delete[] count;
+    delete[] output;
+}
+
+int main() {
+    using namespace std;
+
+    int arr[] = {4, 2, 2, 8, 3, 3, 1};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Original array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    countingSort(arr, size);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
