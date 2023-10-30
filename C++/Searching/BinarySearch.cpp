72598c0008a4f637317a15cf0a77100d06fce3a6
Aryan Sharma <128162387+Aryan741x@users.noreply.github.com>
2023-10-30 23:59:17 +0530

    Create BinarySearch.cpp

---
+++ C++/Searching/BinarySearch.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+int binarySearch(int arr[], int size, int target) {
+    int left = 0;
+    int right = size - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == target)
+            return mid;
+
+        if (arr[mid] < target)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+
+    return -1;
+}
+
+int main() {
+    using namespace std;
+
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int target = 5;
+
+    int result = binarySearch(arr, size, target);
+
+    if (result != -1)
+        cout << "Element found at index: " << result << endl;
+    else
+        cout << "Element not found in the array." << endl;
+
+    return 0;
+}
